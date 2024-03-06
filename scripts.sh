2f1bf7ca4f73c978f725a70517ed893e7cd916ea
rook1e <rook1e404@outlook.com>
2024-03-06 21:13:53 +0800

    fix: fe build cmd

--- scripts.sh
+++ scripts.sh
@@ -8,7 +8,7 @@ build() {
 	cp .env build/
 	go build -o ./build/fusion-server ./cmd/server/* || exit 1
 	echo "building frontend"
-	cd ./frontend && npm run build && cp -R ./build/ $root/build/frontend || exit 1
+	cd ./frontend && npm i && npm run build && cp -R ./build/ $root/build/frontend || exit 1
 	cd $root || exit 1
 }
 
