56c29f770e90d977195787346446d5003a4c4e07
rook1e <rook1e404@outlook.com>
2024-03-12 17:05:17 +0800

    chore: update shadcn-svelte

--- frontend/src/lib/components/ui/alert-dialog/alert-dialog-action.svelte
+++ frontend/src/lib/components/ui/alert-dialog/alert-dialog-action.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { AlertDialog as AlertDialogPrimitive } from "bits-ui";
-	import { buttonVariants } from "$lib/components/ui/button";
-	import { cn } from "$lib/utils";
+	import { buttonVariants } from "$lib/components/ui/button/index.js";
+	import { cn } from "$lib/utils.js";
 
 	type $$Props = AlertDialogPrimitive.ActionProps;
 	type $$Events = AlertDialogPrimitive.ActionEvents;
