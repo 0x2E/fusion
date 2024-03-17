86fc2b81b34691ecf9d337b38a3a2e8ef399f303
rook1e <rook1e404@outlook.com>
2024-03-17 20:13:03 +0800

    feat: show version in page footer

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -1,7 +1,16 @@
-<footer class="mt-4 py-3 bg-muted">
-	<p class="text-muted-foreground text-center">
-		Fusion is an <a href="https://github.com/0x2e/fusion" target="_blank" class="underline">
-			Open Source Project</a
-		>. Logo by <a href="https://icons8.com" target="_blank" class="underline">Icons8</a>
-	</p>
+<script lang="ts">
+	const version = import.meta.env.FUSION.version;
+</script>
+
+<footer class="mt-8 py-3 bg-muted text-muted-foreground">
+	<div class="flex justify-between max-w-[900px] mx-auto px-4">
+		<span>
+			Fusion version <a href="https://github.com/0x2e/fusion" target="_blank" class="underline">
+				{version}</a
+			>.
+		</span>
+		<span>
+			Logo by <a href="https://icons8.com" target="_blank" class="underline">Icons8</a>
+		</span>
+	</div>
 </footer>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { execSync } from 'child_process';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	define: {
+		'import.meta.env.FUSION': JSON.stringify({
+			version:
+				execSync('git describe --tags --abbrev=0').toString().trimEnd() ||
+				execSync('git rev-parse --short HEAD').toString().trimEnd()
+		})
+	},
 	server: {
 		proxy: {
 			'/api': {
