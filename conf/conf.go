41784ec6818d95750065b43ab91c2b31f393b4e1
Michael Lynch <git@mtlynch.io>
2025-01-02 20:24:36 -0500

    Make conf settings read-only
    It feels a bit messy that the entire program has write access to the configuration as a shared global object. Shared globals make it more difficult to reason about a program's behavior.
    
    This rewrite reduces the problem a bit by making the shared global state read-only after the client calls conf.Load.

--- conf/conf.go
+++ conf/conf.go
@@ -16,16 +16,23 @@ const (
 	dotEnvFilename = ".env"
 )
 
-var Conf struct {
-	Host     string `env:"HOST" envDefault:"0.0.0.0"`
-	Port     int    `env:"PORT" envDefault:"8080"`
-	Password string `env:"PASSWORD"`
-	DB       string `env:"DB" envDefault:"fusion.db"`
-
+var conf struct {
+	Host         string `env:"HOST" envDefault:"0.0.0.0"`
+	Port         int    `env:"PORT" envDefault:"8080"`
+	Password     string `env:"PASSWORD"`
+	DB           string `env:"DB" envDefault:"fusion.db"`
 	SecureCookie bool   `env:"SECURE_COOKIE" envDefault:"false"`
 	TLSCert      string `env:"TLS_CERT"`
 	TLSKey       string `env:"TLS_KEY"`
 }
+
+func Host() string       { return conf.Host }
+func Port() int          { return conf.Port }
+func Password() string   { return conf.Password }
+func DB() string         { return conf.DB }
+func SecureCookie() bool { return conf.SecureCookie }
+func TLSCert() string    { return conf.TLSCert }
+func TLSKey() string     { return conf.TLSKey }
 
 func Load() {
 	if err := godotenv.Load(dotEnvFilename); err != nil {
@@ -36,27 +43,27 @@ func Load() {
 	} else {
 		log.Printf("read configuration from %s", dotEnvFilename)
 	}
-	if err := env.Parse(&Conf); err != nil {
+	if err := env.Parse(&conf); err != nil {
 		panic(err)
 	}
 	if err := validate(); err != nil {
 		panic(err)
 	}
 	if Debug {
-		fmt.Println(Conf)
+		fmt.Println(conf)
 	}
 }
 
 func validate() error {
-	if Conf.Password == "" {
+	if conf.Password == "" {
 		return errors.New("password is required")
 	}
 
-	if (Conf.TLSCert == "") != (Conf.TLSKey == "") {
+	if (conf.TLSCert == "") != (conf.TLSKey == "") {
 		return errors.New("missing TLS cert or key file")
 	}
-	if Conf.TLSCert != "" {
-		Conf.SecureCookie = true
+	if conf.TLSCert != "" {
+		conf.SecureCookie = true
 	}
 
 	return nil
