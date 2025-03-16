4ce621ee86723716321fa8a984225e488d6fc368
pompydev <pompydev@proton.me>
2025-03-16 18:14:04 +0900

    feat: make placeholder element size closer to actual element

--- frontend/src/lib/components/ItemListPlaceholder.svelte
+++ frontend/src/lib/components/ItemListPlaceholder.svelte
@@ -1,7 +1,7 @@
 <div class="flex flex-col gap-1">
-	<div class="skeleton h-12 w-full rounded"></div>
-	<div class="skeleton h-12 w-full rounded"></div>
-	<div class="skeleton h-12 w-full rounded"></div>
-	<div class="skeleton h-12 w-full rounded"></div>
-	<div class="skeleton h-12 w-full rounded"></div>
+	<div class="skeleton h-10 w-full rounded"></div>
+	<div class="skeleton h-10 w-full rounded"></div>
+	<div class="skeleton h-10 w-full rounded"></div>
+	<div class="skeleton h-10 w-full rounded"></div>
+	<div class="skeleton h-10 w-full rounded"></div>
 </div>
