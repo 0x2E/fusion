5690a662874aff3ad7de558c9e631206316834c3
rook1e <rook1e404@outlook.com>
2025-04-30 14:51:49 +0800

    fix: trigger load re-run when item filter updates

--- frontend/src/routes/(authed)/bookmarks/+page.ts
+++ frontend/src/routes/(authed)/bookmarks/+page.ts
@@ -1,7 +1,9 @@
 import { listItems, parseURLtoFilter } from '$lib/api/item';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ url }) => {
+export const load: PageLoad = async ({ url, depends }) => {
+	depends('app:page');
+
 	const filter = parseURLtoFilter(url.searchParams, {
 		unread: undefined,
 		bookmark: true,
--- frontend/src/routes/(authed)/feeds/[id]/+page.ts
+++ frontend/src/routes/(authed)/feeds/[id]/+page.ts
@@ -4,7 +4,9 @@ import type { PageLoad } from './$types';
 
 export const prerender = false;
 
-export const load: PageLoad = async ({ url, params }) => {
+export const load: PageLoad = async ({ url, params, depends }) => {
+	depends('app:page');
+
 	const id = parseInt(params.id);
 	const feed = getFeed(id);
 	const filter = parseURLtoFilter(url.searchParams, {
